510c7a443699bbf714f5cba5e11872f9a6e6cb9a
Destructo <rich@cs.ucsb.edu>
2025-05-09 09:29:55 -0700

    fixed inverted CDF

--- weibull.c
+++ weibull.c
@@ -9,6 +9,23 @@
 
 
 
+
+/*
+ * parameters: a, b
+ * CDF: 1 - exp((x/a))^b
+ */
+
+
+double InvertedWeibullCDF(double y, double a, double b)
+{
+	double x = a * pow(-1.0*log(1.0-y),1.0/b);
+	return(x);
+}
+
+#ifdef STANDALONE
+
+#define Usage "weibull -a a -b b [-c sample_count | -q quantile] [-V]\n"
+#define ARGS "a:b:c:Vq:"
 double a;
 double b;
 int Samples;
@@ -16,26 +33,9 @@ double Low;
 double High;
 int Values;
 int CDF;
+double Quantile;
 
-/*
- * parameters: a, b
- * CDF: 1 - exp((x/a))^b
- */
-
-#define Usage "weibull -a a -b b -c sample_count [-V]\n"
-
-#define ARGS "a:b:c:V"
-
-double InvertedWeibullCDF(double y, double a, double b)
-{
-	double x = a * pow(-1.0*log(1.0-y),-1.0*b);
-	return(x);
-}
-
-#ifdef STANDALONE
-
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int c;
 	int i;
@@ -55,6 +55,7 @@ main(int argc, char *argv[])
 	b = 1.0;
 	Samples = -1;
 	Values = 0;
+	Quantile = -1;
 
 	while((c = getopt(argc,argv,ARGS)) != EOF)
 	{
@@ -72,6 +73,9 @@ main(int argc, char *argv[])
 			case 'V':
 				Values = 1;
 				break;
+			case 'q':
+				Quantile = atof(optarg);
+				break;
 			default:
 				fprintf(stderr,"unrecognize arg: %c\n",c);
 				fprintf(stderr,"usage: %s\n",Usage);
@@ -80,18 +84,28 @@ main(int argc, char *argv[])
 		}
 	}
 
-	if(Samples < 1)
+	if((Samples < 1) && (Quantile < 0))
 	{
 		fprintf(stderr,"usage: %s\n",Usage);
 		fflush(stderr);
 		exit(1);
 	}
 
+	if((Samples > 0) && (Quantile > 0) && (Quantile < 1)){
+		fprintf(stderr,
+		"specify either sample count or quantile of interest\n");
+		exit(1);
+	}
+
 	/*
  	 * only generates a random sample at this point
  	 */
-	for(i=0; i < Samples; i++ ) {
-		printf("%f\n",InvertedWeibullCDF(RAND(),a,b));
+	if(Quantile > 0) {
+		printf("%f\n",InvertedWeibullCDF(Quantile,a,b));
+	} else {
+		for(i=0; i < Samples; i++ ) {
+			printf("%f\n",InvertedWeibullCDF(RAND(),a,b));
+		}
 	}
 
 
